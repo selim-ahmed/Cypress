19a4751023edbe9fcfcd921e1ad24eea8088cca6
Selim <selimahmed1305@gmail.com>
2022-03-15 12:21:52 +0600

    cypress project setup

---
+++ cypress/integration/auth/login.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+<reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit('http://103.4.145.251:3028/')
+  })
+
+  it('displays two todo items by default', () => {
+    cy.get('.btn').click()
+    cy.get(':nth-child(1) > .form-control').type('admin')
+//    cy.get('')
+
+  })
+
+ 
+
+
+})
